bcd2060c2f07a956cf030c5f24f9d36257e5c95e
Vojtech Prikryl <devel@aldred.cz>
2018-02-21 18:36:19 +0100

    trackAction ⛹️‍♂️

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,23 @@
 const BE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:8080';
 
-export const getMetadata = () => fetch(`${BE_URL}/metadata`).then(r => r.json());
+export const getMetadata = () =>
+  fetch(`${BE_URL}/metadata`)
+    .then(r => r.json())
+    .then(metadata => ({
+      ...metadata,
+      spaces: metadata.spaces.map(({ id }: any) => ({ id, name: `space[${id}]` })),
+    }));
+
+export const trackAction = (data: { action: string; duration: number }) => {
+  fetch(`${BE_URL}/track`, {
+    body: JSON.stringify({
+      ...data,
+      userId: 1,
+      spaceId: 1,
+    }),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+};
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,28 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import { getMetadata } from './api';
+import { getMetadata, trackAction } from './api';
 import MetadataStore from './stores/MetadataStore';
 import App from './App';
 
 import './index.css';
 
+const TIME_START = Date.now();
+
 getMetadata().then(metadata => {
   MetadataStore.setMetadata(metadata);
 
+  trackAction({
+    action: 'DATA_LOADED',
+    duration: (Date.now() - TIME_START) * 9,
+  });
+
   ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+
+  trackAction({
+    action: 'CHARTS_RENDERED',
+    duration: (Date.now() - TIME_START) * 9,
+  });
 });
 
 registerServiceWorker();
