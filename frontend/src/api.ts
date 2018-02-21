5894c7f51aaa76854571828dc502dd775adf697a
Vojtech Prikryl <devel@aldred.cz>
2018-02-21 18:09:10 +0100

    Fetching Metadata fix ENV var 😔

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,3 @@
-const BE_URL = process.env.BE_URL || 'http://localhost:8080';
-console.log(BE_URL);
+const BE_URL = process.env.REACT_APP_BE_URL || 'http://localhost:8080';
+
 export const getMetadata = () => fetch(`${BE_URL}/metadata`).then(r => r.json());
