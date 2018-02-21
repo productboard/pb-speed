2673b9608b4f20d8f7607641b3446367137bca75
Tomas Ruzicka <me@tomasr.com>
2018-02-21 11:18:46 +0100

    add all base routes + tests

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,30 @@
 const Koa = require('koa');
 const app = (module.exports = new Koa());
+const router = require('koa-router')();
 
-app.use(async function(ctx) {
+async function index(ctx) {
   ctx.body = '<html />';
-});
+}
+
+async function track(ctx) {
+  ctx.status = 201;
+  ctx.body = {ok: true};
+}
+
+async function data(ctx) {
+  ctx.status = 200;
+  ctx.body = {
+    data: [
+      {id :1, value: 2, time: 123456789}
+    ]
+  }
+}
+
+router
+  .get('/', index)
+  .get('/data', data)
+  .post('/track', track);
+
+app.use(router.routes());
 
 if (!module.parent) app.listen(3000);
--- backend/test.js
+++ backend/test.js
@@ -2,15 +2,35 @@ const app = require('./server');
 const server = app.listen();
 const request = require('supertest').agent(server);
 
-describe('Hello World', function() {
-  after(function() {
+describe('Hello World', () => {
+  after(() => {
     server.close();
   });
 
-  it('should serve SPA', function(done) {
+  it('should serve SPA', done => {
     request
       .get('/')
       .expect(200)
       .expect('<html />', done);
+  });
+
+  it('should serve graph data', done => {
+    request
+      .get('/data')
+      .expect(200)
+      .expect({data: [{id: 1, value: 2, time: 123456789}]}, done);
+  });
+
+  it('should save tracked data', done => {
+    request
+      .post('/track')
+      .expect(201)
+      .expect({ok: true}, done);
+  });
+
+  it('should reject non-existing routes', done => {
+    request
+      .get('/trolo')
+      .expect(404, done);
   });
 });
